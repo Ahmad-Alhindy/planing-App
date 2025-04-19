24268db3e802d2cee28b10af8ba35427921f652e
Ahmad Alhindy <alah23xc@student.ju.se>
2025-04-19 06:33:20 +0200

    changing the x to a delete Icon in the appointments list fore a specific day

--- app/src/main/java/com/example/planingapp/AppointmentDetails.kt
+++ app/src/main/java/com/example/planingapp/AppointmentDetails.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -119,7 +120,7 @@ fun AppointmentDetails(
                                         }
                                     ) {
                                         Icon(
-                                            imageVector = Icons.Default.Close,
+                                            imageVector = Icons.Default.Delete,
                                             contentDescription = "Delete",
                                             tint = Color.Red
                                         )
