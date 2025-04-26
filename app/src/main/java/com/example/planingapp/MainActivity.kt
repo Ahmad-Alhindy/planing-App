e6dfddb3d04ab377cbc667499b4e478830f90b05
Ahmad Alhindy <alah23xc@student.ju.se>
2025-04-26 03:00:48 +0200

    Update MainActivity.kt

--- app/src/main/java/com/example/planingapp/MainActivity.kt
+++ app/src/main/java/com/example/planingapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.planingapp
 
+import android.app.Application
 import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -9,20 +10,27 @@ import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavType
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.planingapp.ui.theme.PlaningAppTheme
 import androidx.navigation.compose.NavHost
+import androidx.navigation.navArgument
 import androidx.room.Room
 import com.example.planingapp.db.AppointmentDb
 import com.example.planingapp.logic.AppointmentViewModel
+import com.example.planingapp.logic.SettingsScreen
+import com.example.planingapp.logic.UserPreferencesViewModel
 import com.example.planingapp.logic.createNotificationChannel
 import com.example.planingapp.logic.nav
 import com.example.planingapp.views.MakeAppointment
 import com.example.planingapp.views.CalendarScreen
-import com.example.planingapp.views.DayView
 import com.example.planingapp.views.HomeScreen
 import com.example.planingapp.views.WeekView
+import java.time.LocalDate
 
 
 class MainActivity : ComponentActivity() {
@@ -50,18 +58,31 @@ class MainActivity : ComponentActivity() {
                         composable(nav.homeScreen) {
                             HomeScreen(navController = navController)
                         }
-                        composable(nav.addNote) {
-                            MakeAppointment(viewModel = viewModel, navController = navController)
-                        }
                         composable(nav.calander) {
                             CalendarScreen(viewModel = viewModel, navController = navController)
                         }
                         composable(nav.week) {
                             WeekView(navController = navController, viewModel = viewModel)
                         }
-                        composable(nav.day) {
-                            DayView(navController = navController)
+                        composable(
+                            route = "makeAnAppointment?date={date}",
+                            arguments = listOf(
+                                navArgument("date") {
+                                    type = NavType.StringType
+                                    nullable = true
+                                    defaultValue = null
+                                }
+                            )
+                        ) { backStackEntry ->
+                            val dateString = backStackEntry.arguments?.getString("date")
+                            val date = dateString?.let { LocalDate.parse(it) }
+
+                            MakeAppointment(viewModel, navController, date)
                         }
+                        composable(nav.settings) {
+                            SettingsScreen(userViewModel)
+                        }
+
                     })
             }
         }
