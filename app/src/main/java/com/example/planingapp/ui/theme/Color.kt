d6fd79df5282d352b2d09a788ab4ba3ae47dc24e
Ahmad Alhindy <alah23xc@student.ju.se>
2025-04-28 17:02:22 +0200

    edit some color to use it in the app

--- app/src/main/java/com/example/planingapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/planingapp/ui/theme/Color.kt
@@ -1,10 +1,14 @@
 package com.example.planingapp.ui.theme
 
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
+
+val buttonsL = Color(0xFF4CAF50)
+val buttonsD = Color(0xFF04A2B4)
 
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
--- app/src/main/java/com/example/planingapp/ui/theme/Theme.kt
+++ app/src/main/java/com/example/planingapp/ui/theme/Theme.kt
@@ -1,25 +1,25 @@
 package com.example.planingapp.ui.theme
 
-import android.app.Activity
 import android.os.Build
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import com.example.planingapp.logic.SettingsManger.Companion.isDarkMode
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
+    primary = buttonsD,
+    secondary = Color.White,
     tertiary = Pink80
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
+    primary = buttonsL,
+    secondary = Color.Black,
     tertiary = Pink40
 
     /* Other default colors to override
@@ -35,9 +35,10 @@ private val LightColorScheme = lightColorScheme(
 
 @Composable
 fun PlaningAppTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
+    darkTheme: Boolean = isDarkMode.value,
+
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
